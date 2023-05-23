bd5a4c31da6cb5d6c9b8748fb3a615d796ae0753
AceDemo1 <122922501+AceDemo1@users.noreply.github.com>
2023-05-23 14:00:28 -0500

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,7 @@
 
 int main(void)
 {
-	write(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(stderr,quo,59);
 	return (1);
 }
