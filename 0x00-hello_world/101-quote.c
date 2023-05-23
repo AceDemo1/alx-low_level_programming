1597b2e9e2392c226e752570dc1f215439e3e136
AceDemo1 <122922501+AceDemo1@users.noreply.github.com>
2023-05-23 17:54:48 -0500

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-    char gg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    char gg[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
     fprintf(stderr, "%s", gg);
     return 1;
