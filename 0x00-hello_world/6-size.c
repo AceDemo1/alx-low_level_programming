139db591d138e075a3a6467e0876e6bd643d6522
AceDemo1 <122922501+AceDemo1@users.noreply.github.com>
2023-05-22 23:46:04 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: using sizeof to print the size of various types.
+ * Description: Using sizeof to print the size of various types.
  *
- * Return: Always 0 (Succesful)
-*/
-
-
-int main() {
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
     printf("Size of an int: %zu byte(s)\n", sizeof(int));
     printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
