262b16e1832e1c19bc4b4f33c7e5519b0c3537d2
AceDemo1 <lawal641@gmail.com>
2023-05-25 06:59:39 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,10 +17,10 @@ int main(void)
 	{
 		/*convert digit to ASCII representation*/
 		putchar(digit + '0');
-		if (digit !=9)
+		if (digit !=9 )
 		{
 		putchar(',');
-		putchar(' ');		
+		putchar(' ');
 		}
 		digit++;
 	}
