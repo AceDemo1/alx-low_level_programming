78cf750885fd9ba7aae4ca99037bdf9c61210633
AceDemo1 <lawal641@gmail.com>
2023-05-28 15:34:30 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ */
+
+int _islower(int c)
+{
+	char al[] = "abcdefghijklmnopqrstuvwxyz"
+	char AL[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	int i = 0;
+	int j = 0;
+
+	if (c == al[i])
+	{
+		return 1;
+	}
+	else if (c == AL[j])
+	{
+		return 0;
+	}
+}
