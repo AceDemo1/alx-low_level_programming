b7876c7936c49256dc04a8a8d46f01f9df2563cb
AceDemo1 <lawal641@gmail.com>
2023-05-28 09:47:43 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
 * main -Entry point
-* 
+*
 * Description - prints _putchar
 *
 * Return: Always 0 (sucess)
@@ -15,7 +15,7 @@ int main(void)
 char a[] = "_putchar";
 int i = 0;
 int j = strlen(a);
-while(i < j)
+while (i < j)
 {
 _putchar(a[i]);
 i++;
@@ -23,5 +23,5 @@ i++;
 
 _putchar('\n');
 
-return (0);	
+return (0);
 }
