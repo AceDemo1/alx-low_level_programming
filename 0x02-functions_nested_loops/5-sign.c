698e70ac33499eeea2c9d13be33481bb66328211
AceDemo1 <lawal641@gmail.com>
2023-05-28 17:04:14 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,7 @@
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
  *
- * @c: takes input from other functions.
- *
+ * @n: takes integer type input for function.
  * Return: 1 is c if true else 0
 */
 
@@ -14,17 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+' + 0);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0' + 0);
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-' + 0);
+		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
