55fdbb5d2d794f609dc405d6aec258f8776ff96d
AceDemo1 <lawal641@gmail.com>
2023-05-29 08:48:13 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,16 @@
 
 /**
  * add - adds two integers and returns the result
+ * @a: first input
+ * @b: second input
+ *
  * Return: sum
+ *
 */
 
 int add(int m, int n)
 {
 	int sum;
+	
 	return (sum = m + n);
 }
