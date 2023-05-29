bb117c9f9816a3b85ebcf1bcaebd26cb8f653a9b
AceDemo1 <lawal641@gmail.com>
2023-05-29 09:10:19 -0700

    prints all natural numbers from n to 98, followed by a new line

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ *
+ * @n: first input
+ *
+*/
+
+void print_to_98(int n)
+{       
+	while (n <= 98)
+	{
+		_putchar('n')
+		n++;
+	}
+	_putchar('\n')
+}
