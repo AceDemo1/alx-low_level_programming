c1c2310d48c663dc428b0d47a95a3dcbb5764d9c
AceDemo1 <lawal641@gmail.com>
2023-05-28 17:13:25 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/**
+ * _abs - function that computes the absolute
+ *       value of an integer
+ *
+ * @n: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int r)
+{
+	int r;
+	if (r == -r)
+	{
+		_putchar('r');
+	}
+	return (0);
+
+
+}
