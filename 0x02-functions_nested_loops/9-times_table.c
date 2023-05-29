3ca27efd22b6ef74b212d1b48c09e3de7c0e7593
AceDemo1 <lawal641@gmail.com>
2023-05-29 05:15:58 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,8 @@ void times_table(void)
 		while (j <= 9)
 		{
 			prod = i * j;
-			_putchar(prod + '0');
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 			if (j !=9)
 			{
 			_putchar(',');
