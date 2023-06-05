067e4fc6c50dd02bbb1a7784bed461857abdd541
AceDemo1 <lawal641@gmail.com>
2023-06-05 09:58:36 -0700

    convert to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,3 +8,25 @@
  */
 int _atoi(char *s)
 {
+	int si;
+	int re = 0;
+
+	while (*s == ' ')
+		s++;
+	if (*s == '-')
+	{
+		si = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		re = re * 10 + (*s - '0');
+		s++;
+	}
+	return (result * si);
+}
+
