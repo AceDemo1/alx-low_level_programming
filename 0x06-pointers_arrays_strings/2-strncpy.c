643a1996601212c0ba89e569f65d51370b126a51
AceDemo1 <lawal641@gmail.com>
2023-06-06 09:51:54 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (src[i] != '\0')
+	{
+
+		dest[len] = src[len];
+		len++;
+	}
+	while (len < n)
+	{
+		dest[i] = '\0';
+		len++;
+	}
+	return (dest);
+}
