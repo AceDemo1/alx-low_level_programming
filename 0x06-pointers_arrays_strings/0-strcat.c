e7fc740ad592b190f3ebb727df0a9af27ccd4727
AceDemo1 <122922501+AceDemo1@users.noreply.github.com>
2023-06-06 04:05:15 -0500

    Update 0-strcat.c
    Signed-off-by: AceDemo1 <122922501+AceDemo1@users.noreply.github.com>

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,7 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *ptr = dest;
 	while (*dest != '\0')
 	{
 		dest++;
@@ -19,5 +20,6 @@ char *_strcat(char *dest, char *src)
 		dest++;
 		src++;
 	}
+	*ptr = '\0';
 	return (dest);
 }
