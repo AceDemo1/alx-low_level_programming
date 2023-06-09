c0343293d6c7315cfeda395a52467555086b7e59
AceDemo1 <lawal641@gmail.com>
2023-06-08 19:49:36 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,47 @@
 /**
- * _strcmp - Compares two strings
- * @s1: first string
- * @s2: second string
+ * _strcmp - a function that compares two strings
  *
- * Return: Difference between the ASCII
- *         values of the first differing characters.
- */
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
+ *
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0;
+	int i = 0, r;
 
-        while (s1[i] != '\0' && s2[i] != '\0')
-        {
-                if (s1[i] != s2[i])
-                        return (s1[i] - s2[i]);
+	/**
+	 * iterate through src and
+	 * compare it with dest
+	*/
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		/**
+		 * if they differ by a single character
+		 * don't iterate further
+		*/
+		if (s1[i] != s2[i])
+		{
+			/*
+			 * return the difference between
+			 * the two characters
+			*/
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
+	}
 
-                i++;
-        }
-
-        return (s1[i] - s2[i]);
+	return (r);
 }
-
