57cab7e8cbd02fc20d9a0b88543feffdaf50108b
AceDemo1 <lawal641@gmail.com>
2023-06-08 19:48:42 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * @s2: second string
  *
  * Return: Difference between the ASCII
- * values of the first differing characters.
+ *         values of the first differing characters.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,11 +13,11 @@ int _strcmp(char *s1, char *s2)
         while (s1[i] != '\0' && s2[i] != '\0')
         {
                 if (s1[i] != s2[i])
-                {
-			return (s1[i] - s2[i]);
-                        break;
-                }
+                        return (s1[i] - s2[i]);
+
                 i++;
         }
+
         return (s1[i] - s2[i]);
 }
+
