6220302e068a9bb26eaaac4dcf489caeb90b5136
AceDemo1 <lawal641@gmail.com>
2023-06-08 19:19:11 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-**
-=======
 /**
->>>>>>> 68338275640506d416f26299e347bf9a3e4be58a
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
  *
- * Return: 0 if strings are equal, negative if s1 is less than s2, positive if s1 is greater than s2
+ * Return: return (s1[i] - s2[i]) if strings are equal, negative if s1 is less than s2, positive if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -17,18 +13,10 @@ int _strcmp(char *s1, char *s2)
     {
 	    if (s1[i] != s2[i])
         {
-<<<<<<< HEAD
-		return (s1[i] - s2[i]);
-	}
-	i++;
-    }
-	return s1[i] - s2[i];
-=======
             return (s1[i] - s2[i]);
             break;
         }
         i++;
     }
    return (s1[i] - s2[i]);
->>>>>>> 68338275640506d416f26299e347bf9a3e4be58a
 }
