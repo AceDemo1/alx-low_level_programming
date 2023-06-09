fc605e2f8ed9733661a520f71032d37d8149d856
AceDemo1 <lawal641@gmail.com>
2023-06-08 23:30:38 -0700

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,17 @@ char *cap_string(char *str)
 {
     int i;
 
-    // Capitalize first letter of the string
     if (str[0] >= 'a' && str[0] <= 'z')
         str[0] = str[0] - 32;
 
-    // Iterate through the rest of the string
     for (i = 1; str[i] != '\0'; i++)
     {
-        // Check if current character is a separator
         if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
             str[i] == ',' || str[i] == ';' || str[i] == '.' ||
             str[i] == '!' || str[i] == '?' || str[i] == '"' ||
             str[i] == '(' || str[i] == ')' || str[i] == '{' ||
             str[i] == '}')
         {
-            // Capitalize the next character if it is a lowercase letter
             if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
                 str[i + 1] = str[i + 1] - 32;
         }
