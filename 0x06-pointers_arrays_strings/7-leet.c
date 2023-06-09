ce3ed99a38dbff9d33a63b42ba5a7b589e1444e4
AceDemo1 <lawal641@gmail.com>
2023-06-09 00:27:48 -0700

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+/**
+ * leet - a function that encodes a string into 1337
+ *
+ * @s: string input
+ *
+ * Return: @s
+*/
+
+char *leet(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	char a[] = "aeotl";
+	char b[] = "AEOTL";
+	char c[] = "43071";
+
+	while (s[i] != '\0')
+	{
+		while (j < 5)
+		{
+			if (s[i] == a[j] || s[i] == b[j])
+			{
+				s[i] == c[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
+
+
