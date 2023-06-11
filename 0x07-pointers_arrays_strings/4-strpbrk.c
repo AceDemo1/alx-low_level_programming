0b35ba9b723058ef5209df57af8748ab9398ab87
AceDemo1 <lawal641@gmail.com>
2023-06-11 09:58:30 -0700

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,10 +9,11 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
