5979b33aa437fd08a01115a1b12423ebfbd5b2b9
AceDemo1 <lawal641@gmail.com>
2023-06-11 13:06:39 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,9 +13,8 @@ char *_strstr(char *haystack, char *needle)
 	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (needle[j] != '\0' && haystack[i] == needle[j])
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
 		{
-			i++;
 			j++;
 		}
 		if (needle[j] == '\0')
