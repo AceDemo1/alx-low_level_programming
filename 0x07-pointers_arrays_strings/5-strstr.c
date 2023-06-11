d7bdbc6da779d37a3f07871171b2d81e5f760f3b
AceDemo1 <lawal641@gmail.com>
2023-06-11 10:52:10 -0700

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+/**
+* _strstr - function locate
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i] == needle[j])
+		{
+			i++;
+			j++;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+		i++;
+	}
+	return ('\0');
+}
