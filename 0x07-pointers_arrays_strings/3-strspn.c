3645cc1e032885eb1b9a15bebea14b408f7a118e
AceDemo1 <122922501+AceDemo1@users.noreply.github.com>
2023-06-11 15:42:38 +0100

    Update 3-strspn.c
    Signed-off-by: AceDemo1 <122922501+AceDemo1@users.noreply.github.com>

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,26 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f;
+    int z = 0, x, y;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		j = 0;
-		f = 1; /*flag status*/
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				f = 0; /*success*/
-				break;
-			}
-			j++;
-		}
-		if (f == 1)
-			break;
-		i++;
-	}
+    for (x = 0; s[x] != '\0'; x++)
+    {
+        for (y = 0; accept[y] != '\0'; y++)
+        {
+            if (s[x] == accept[y])
+            {
+                z++;
+                break;
+            }
+        }
+        if (accept[y] == '\0')
+            return z;
+    }
 
-	return (i);
+    return z;
 }
