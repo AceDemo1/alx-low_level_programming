f3e88ce6619d626ee365f434c8bda351ebba3d6b
AceDemo1 <lawal641@gmail.com>
2023-06-19 07:52:51 -0700

    a function that concatenates two strings

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: a pointer to a 2 dimensional array of integers.
+ */
+int **alloc_grid(int width, int height)
+{
+	char **arr2;
+	
+	arr2 = malloc (sizeof(char) *  );
+	
+	return (arr2);
+}
