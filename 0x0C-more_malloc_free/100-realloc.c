34e14ea3b431d6824cc8edd27e4bf1d3ae07797d
AceDemo1 <lawal641@gmail.com>
2023-06-21 05:42:26 -0700

    a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		for (i = 0; i < old_size; i++)
 			d[i] = s[i];
+	free(ptr);
+	return (newall);
 	}
-		free(ptr);
-		return (newall);
+:wq
 }
