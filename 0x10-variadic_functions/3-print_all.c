34db390b48a91df37f1261c12457d9a6a4b331c9
AceDemo1 <lawal641@gmail.com>
2023-07-03 07:28:45 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything(any data type)
+ * print_all - prints anything (any data type)
  * @format: pointer to string of data format types
  */
 void print_all(const char * const format, ...)
@@ -11,26 +11,38 @@ void print_all(const char * const format, ...)
 	va_list a;
 
 	va_start(a, format);
-	while (format[i])
- 	{
-		if(format[i] == 'c')
-			printf("%c", va_arg(a, int));
-	 	else if (format[i] == 'i')
- 			printf("%d", va_arg(a, int));
-		else if(format[i] == 'f')
-			printf("%f", va_arg(a, double));
-		else if (format[i] == 's')
+	while (format && format[i])
+	{
+		if (format[i] == 'c')
 		{
-			str = va_arg(a, char*);
-			if (str == NULL)
-				printf("(nil)");
- 			else
- 				printf("%s",str);
+			printf("%c", va_arg(a, int));
+			i++;
+			continue;
 		}
- 		if (format[i + 1] && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
-				printf(", ");
+		if (format[i] == 'i')
+		{
+			printf("%d", va_arg(a, int));
+			i++;
+			continue;
+		}
+		if (format[i] == 'f')
+		{
+			printf("%f", va_arg(a, double));
+			i++;
+			continue;
+		}
+		if (format[i] == 's')
+		{
+			str = va_arg(a, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			i++;
+			continue;
+		}
 		i++;
 	}
 	printf("\n");
 	va_end(a);
 }
+
