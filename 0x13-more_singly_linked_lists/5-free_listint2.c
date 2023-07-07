abed6f02bf6414a26e3ee7df7e4ec4af377530b3
AceDemo1 <lawal641@gmail.com>
2023-07-07 14:28:40 -0700

    a function that frees a listint_t list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		free(temp);
+		temp = temp->next;
+	}
+	*head = NULL;
+}
