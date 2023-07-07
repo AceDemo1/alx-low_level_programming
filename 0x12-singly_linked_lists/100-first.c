36045d2109edea2ad8be783956ac8cbfe0cf89e6
AceDemo1 <lawal641@gmail.com>
2023-07-07 03:13:18 -0700

    execute a function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * bmain - function executed before main
  * Return: no return.
 **/
-void bmain(void) __attribute__((constructor))
+void bmain(void) __attribute__((constructor));
+void bmain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
