8c4942ceb0f0bd2fdebeff186b12ddfc76140bdb
AceDemo1 <lawal641@gmail.com>
2023-07-07 03:25:28 -0700

    execute a function before main'? (y/n): printing in assembly (64-bit program)

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 0
+
+section .text
+    extern printf
+
+global main
+main:
+    ; Save callee-saved registers
+    push rbp
+    mov rbp, rsp
+
+    ; Pass the string address as the argument to printf
+    mov rdi, format
+    mov rsi, hello
+    xor eax, eax  ; Clear EAX for variadic functions
+    call printf
+
+    ; Clean up the stack
+    mov rsp, rbp
+    pop rbp
+
+    ; Exit the program
+    mov eax, 0
+    ret
+
