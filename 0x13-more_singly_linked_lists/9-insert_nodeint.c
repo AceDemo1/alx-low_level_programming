9891e98ef05d592280d5995a9a3961314ba7d5ec
AceDemo1 <lawal641@gmail.com>
2023-07-08 12:11:55 -0700

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Insert a new node at a given positiion.
- * @head: First node address.
- * @idx: Position of the new node to be inserted in.
- * @n: Data of the new node.
- * Return: Address of the new node.
- **/
+* insert_nodeint_at_index - Insert a new node at a given positiion.
+* @head: First node address.
+* @idx: Position of the new node to be inserted in.
+* @n: Data of the new node.
+* Return: Address of the new node.
+**/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -22,13 +22,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->n = n;
 	newnode->next = NULL;
 	idx--;
-	while (idx != 1)
-	{
-		temp = temp->next;
-		idx--;
-	}
-	newnode->next = temp->next;
-	temp->next = newnode;
-	return (newnode);
+while (idx != 1)
+{
+temp = temp->next;
+idx--;
 }
-
+newnode->next = temp->next;
+temp->next = newnode;
+return (newnode);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeinta.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - Insert a new node at a given positiion.
+ * @head: First node address.
+ * @idx: Position of the new node to be inserted in.
+ * @n: Data of the new node.
+ * Return: Address of the new node.
+ **/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+        unsigned int i = 0;
+        listint_t *temp = *head;
+        listint_t *newnode;
+
+        if (head == NULL)
+                return (NULL);
+        newnode = malloc(sizeof(listint_t));
+        if (newnode == NULL)
+                return (NULL);
+        newnode->n = n;
+        newnode->next = NULL;
+        if (idx == 0)
+        {
+                newnode->next = *head;
+                *head = newnode;
+                return (newnode);
+        }
+	else
+	{
+        while (i < idx - 1 && temp != NULL)
+        {
+                temp = temp->next;
+                i++;
+		if (temp == NULL)
+			return (NULL);
+        }
+	}
+        newnode->next = temp->next;
+        temp->next = newnode;
+        return (newnode);
+}
