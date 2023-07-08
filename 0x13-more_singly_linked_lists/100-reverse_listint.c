263e5693dda28e1ac1f25c8119699c5ea2df711c
AceDemo1 <lawal641@gmail.com>
2023-07-08 15:11:38 -0700

    Write a function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,13 +13,13 @@ listint_t *reverse_listint(listint_t **head)
 
 	temp1 = NULL;
 	temp2 = NULL;
-	while (*head != NULL))
+	while (*head != NULL)
 	{
-		temp2 = head->next;
-		head->next = temp1;
-		temp1 = head;
-		head =temp2;
+		temp2 = *head->next;
+		*head->next = temp1;
+		temp1 = *head;
+		*head = temp2;
 	}
-	*head = temp;
+	*head = temp2;
 	return (*head);
 }
