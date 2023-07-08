9c32b40693d91b8d0fa5e83241206e8205dee394
AceDemo1 <lawal641@gmail.com>
2023-07-08 16:24:17 -0700

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "lists.h"
 
+const listint_t *find_loop_node(const listint_t *head);
+void print_with_loop(const listint_t *head, const listint_t *loop_node);
+
 /**
  * print_listint_safe - Prints a listint_t linked list.
  * @head: A pointer to the head of the list.
@@ -84,33 +87,27 @@ void print_with_loop(const listint_t *head, const listint_t *loop_node)
 	size_t count = 0;
 	int loop_started = 0;
 
-	while (current != NULL)
+	while (current != loop_node)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
 
 		count++;
 		current = current->next;
-
-		/* Check if the current node is the loop node */
-		if (current == loop_node)
-		{
-			if (loop_started)
-				break;
-			else
-				loop_started = 1;
-		}
 	}
 
-	printf("-> [%p] %d\n", (void *)current, current->n);
+	printf("[%p] %d\n", (void *)current, current->n);
+	printf("-> [%p] %d\n", (void *)current->next, current->next->n);
 	count++;
 
-	while (current->next != loop_node)
+	current = current->next;
+	while (current != loop_node)
 	{
+		printf("[%p] %d\n", (void *)current, current->n);
 		current = current->next;
 		count++;
 	}
 
-	printf("Loop starts at [%p] %d\n", (void *)current, current->n);
+	printf("Loop starts at [%p] %d\n", (void *)loop_node, loop_node->n);
 	printf("Loop size = %lu\n", count);
 }
 
