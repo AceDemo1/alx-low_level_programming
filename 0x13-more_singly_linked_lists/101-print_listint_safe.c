6b7d10fe8e0d3126ffca9fa1c1b5ab3fb5c5d714
AceDemo1 <lawal641@gmail.com>
2023-07-08 16:48:06 -0700

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,119 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
-const listint_t *find_loop_node(const listint_t *head);
-void print_with_loop(const listint_t *head, const listint_t *loop_node);
-
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head of the list.
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
-	const listint_t *loop_node;
-	size_t count = 0;
+	size_t num = 0;
+	long int diff;
 
-	current = head;
-
-	/* Check if the list is empty */
-	if (current == NULL)
-		return (0);
-
-	/* Detect loop using Floyd's cycle detection algorithm */
-	loop_node = find_loop_node(current);
-
-	if (loop_node != NULL)
+	while (head)
 	{
-		print_with_loop(current, loop_node);
-		return (count);
-	}
-
-	/* No loop detected, print the list normally */
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * find_loop_node - Finds the node where the loop begins in a linked list.
- * @head: A pointer to the head of the list.
- *
- * Return: The node where the loop begins, or NULL if there is no loop.
- */
-const listint_t *find_loop_node(const listint_t *head)
-{
-	const listint_t *slow, *fast;
-
-	slow = head;
-	fast = head;
-
-	while (fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
 		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-			return slow;
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
 	}
 
-	return NULL;
+	return (num);
 }
-
-/**
- * print_with_loop - Prints a linked list containing a loop.
- * @head: A pointer to the head of the list.
- * @loop_node: The node where the loop begins.
- */
-void print_with_loop(const listint_t *head, const listint_t *loop_node)
-{
-	const listint_t *current = head;
-	size_t count = 0;
-	int loop_started = 0;
-
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-
-		count++;
-		current = current->next;
-
-		/* Check if the current node is the loop node */
-		if (current == loop_node)
-		{
-			if (loop_started)
-				break;
-			else
-				loop_started = 1;
-		}
-	}
-
-	printf("-> [%p] %d\n", (void *)current, current->n);
-	count++;
-
-	while (current->next != loop_node)
-	{
-		current = current->next;
-		count++;
-	}
-
-	printf("Loop starts at [%p] %d\n", (void *)current, current->n);
-	printf("Loop size = %lu\n", count);
-}
-
