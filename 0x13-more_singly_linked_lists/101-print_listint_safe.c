9de985fd7649008a57e8a85960b77a5377e210f3
AceDemo1 <lawal641@gmail.com>
2023-07-08 16:17:59 -0700

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,33 +4,29 @@
 
 /**
  * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head node of the list.
+ * @head: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow, *fast;
+	const listint_t *current;
 	size_t count = 0;
 
-	slow = head;
-	fast = head;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	current = head;
+	while (current != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
-
-		if (slow == fast)
+		/* Check if the next node points back to a previous node */
+		if (current->next >= current)
 		{
-			printf("-> [%p] %d\n", (void *)slow->next, slow->next->n);
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
 			break;
 		}
+		current = current->next;
 	}
 
-	return count;
+	return (count);
 }
 
