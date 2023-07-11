5d33917e5bdf3b58494816b01e58deb003da1ace
AceDemo1 <lawal641@gmail.com>
2023-07-11 15:25:28 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,49 @@
 #include "main.h"
+
 /**
-* read_textfile - reads a text file and prints it to the standard output
-* @filename: name of the file to be read
-* @letters: number of letters to read and print
-* Return: the number of letters printed, or 0 if it failed
-*/
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t r, w;
-char *b;
-FILE *f;
+	ssize_t r, w;
+	char *b;
+	FILE *f;
 
-if (filename == NULL)
-return (0);
-f = fopen(filename, "r");
-if (f == NULL)
-return (0);
-b = malloc(sizeof(char) * letters);
-if (b == NULL)
-{
-fclose(f);
-return (0);
-}
-r = fread(b, sizeof(char), letters, f);
-if (r == -1)
-{
-fclose(f);
-free(b);
-return (0);
-}
-w = write(STDOUT_FILENO, b, r);
-if (w == -1 || w != r)
-{
-fclose(f);
-free(b);
-return (0);
-}
-fclose(f);
-free(b);
-return (r);
+	if (filename == NULL)
+		return (0);
+
+	f = fopen(filename, "r");
+	if (f == NULL)
+		return (0);
+
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+	{
+		fclose(f);
+		return (0);
+	}
+
+	r = fread(b, sizeof(char), letters, f);
+	if (r == -1)
+	{
+		fclose(f);
+		free(b);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, b, r);
+	if (w == -1 || w != r)
+	{
+		fclose(f);
+		free(b);
+		return (0);
+	}
+
+	fclose(f);
+	free(b);
+	return (r);
 }
 
