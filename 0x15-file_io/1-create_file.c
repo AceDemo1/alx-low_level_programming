ea9641c7e1f0767b435c87fb5a05e5b96a9a3140
AceDemo1 <lawal641@gmail.com>
2023-07-13 13:39:20 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		w = write(f, text_content, i + 1);
+		w = write(f, text_content, (i + 1));
 		if (w == -1)
 		{
 			close(f);
