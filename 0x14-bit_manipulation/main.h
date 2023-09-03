94a7098f173e7b72004e872efd6305a623b65e3a
AceDemo1 <lawal641@gmail.com>
2023-09-03 08:01:22 -0700

    convert_to_deci

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef main
-#define main
+#ifndef MAIN
+#define MAIN
 unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
