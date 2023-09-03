e74d9daeb34f8a4c83060e68552042674c36bb70
AceDemo1 <lawal641@gmail.com>
2023-09-03 08:22:48 -0700

    convert_to_deci

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		else
-		{
-			num = (2 * num) + (b[i] - '0');
-		}
+		num = (2 * num) + (b[i] - '0');
 	}
 	return (num);
 }
