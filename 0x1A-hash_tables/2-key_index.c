655254fb1d1419bb60ccbb5f49e1b52c35336c10
AceDemo1 <lawal641@gmail.com>
2024-05-05 02:51:02 -0700

    hash index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,8 +9,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	 unsigned long int i;
-	 
-	 i = hash_djb2(key) % size;
-	 return(i);
+	unsigned long int i;
+
+	i = hash_djb2(key) % size;
+	return (i);
 }
