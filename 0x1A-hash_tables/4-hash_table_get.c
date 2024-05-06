0cd411c284942328cd1409fd81120030e9b3dd08
AceDemo1 <lawal641@gmail.com>
2024-05-06 04:02:50 -0700

    get hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,18 +9,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int i;
-        hash_node_t *k;
+	unsigned long int i;
+	hash_node_t *k;
 
-        if (!key || !ht)
-                return (NULL);
-        i = key_index((unsigned char *)key, ht->size);
-        k = ht->array[i];
-        while (k)
-        {
-                if (strcmp(k->key, key) == 0)
-                        return (k->value);
-                k = k->next;
-        }
-        return (NULL);
+	if (!key || !ht)
+		return (NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	k = ht->array[i];
+	while (k)
+	{
+		if (strcmp(k->key, key) == 0)
+			return (k->value);
+		k = k->next;
+	}
+	return (NULL);
 }         
